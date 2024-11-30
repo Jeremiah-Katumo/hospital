aeca67e4e4510b7dac381374de8d3b831efffbfc
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-11-30 11:59:17 +0300

    WIP

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var session = require('express-session');
+var cookie = require('cookie-parser');
+var path = require('path');
+var ejs = require('ejs');
+var multer = require('multer');
+var async = require('async');
+var nodemailer = require('nodemailer');
+var crypto = require('crypto');
+var expressValidator = require('express-validator');
+var sweetalert = require('sweetalert2');
+var bodyParser = require('body-parser');
+const http = require('http');
+var dotenv = require('dotenv');
+
+var db = require.main.require('./database/db');
+var signup = require('./controllers/signup');
+
+
+dotenv.config();
+
+var app = express();
+
+app.set('view_engine', 'ejs');
+// const server = http.createServer(app);
+
+app.use(express.static('./public/'));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(cookie());
+app.use('./signup', signup);
+
+const PORT = process.env.PORT || 3000
+
+const server = () => {
+    // db connection
+    db();
+    app.listen(PORT, () => {
+        console.log(`Express is listening on: http://localhost:${PORT}` +
+            '\n' + 'Press Ctrl + C to terminate');
+    }).on('error', (err) => {
+        console.log(err);
+    });
+};
