3fa2034063cd95d887948a9dbc254dbbc6eea2fb
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-12-04 15:53:16 +0300

    added code to reset files

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import session from 'express-session';
-import { signUp } from '../controllers/auth/signup.js';
+import { signUp } from '../controllers/signup.js';
 import { logIn, logOut } from '../controllers/auth/login.js';
-import { verify } from '../controllers/auth/verify.js';
+import { verify } from '../controllers/verify.js';
 import { reset } from '../controllers/auth/reset.js';
 import { validate } from '../validations/index.js';
 import { useSignUpValidator, useLogInValidator } from '../validations/auth.js';
