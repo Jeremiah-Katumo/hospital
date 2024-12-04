482acb689e312e2630d0947e464a35c156d359f4
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-12-04 15:57:00 +0300

    added code to reset files

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -1,52 +1,9 @@
 import { validationResult } from 'express-validator';
-// import { request } from 'http';
 import { successResponse, errorResponse } from '../../helpers/responseHelper.js';
 import {
     findUserByUsername,
 } from '../../services/authService.js';
 
-// conn = mysql.createPool({
-//     host: process.env.HOST,
-//     user: process.env.USER,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE_NAME,
-//     waitForConnections: true,
-//     connectionLimit: 10,
-// });
-
-// export const logIn = (req, res) => {
-//     const errors = validationResult(req);
-//     if (!errors.isEmpty()) {
-//         return res.status(422).json({errors: errors.array()});
-//     }
-
-//     var username = request.body.username;
-//     var password = request.body.password;
-//     if (username && password) {
-//         conn.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-//             if (results.length > 0) {
-//                 request.session.loggedin = true;
-//                 request.session.username = username;
-//                 response.cookie('username', username);
-//                 var status = results[0].email_status;
-
-//                 if (status == "not_verified") {
-//                     response.send('Please make sure you verify your email!')
-//                 } else {
-//                     sweetAlert.fire('Logged In');
-//                     response.redirect('/home');
-//                 }
-//             } else {
-//                 response.send('Incorrect user credentials!');
-//             }
-
-//             response.end()
-//         })
-//     } else {
-//         response.send('Please enter username and password!');
-//         response.end();
-//     }
-// }
 
 export const logIn = async (req, res) => {
     // Validate the request
--- server/controllers/auth/signup.js
+++ server/controllers/auth/signup.js
@@ -9,60 +9,6 @@ import {
     getUserIdByEmail,
 } from '../../services/authService.js';
 
-// export const signUp = (req, res) => {
-//     const errors = validationResult(req);
-//     if (!error.isEmpty()) {
-//         return res.status(422).json({error: errors.array()});
-//     }
-
-//     var emailStatus = "not_verified";
-//     var email = req.body.email;
-//     var username = req.body.username;
-
-//     db.signup(req.body.username, req.body.email, req.body.password, emailStatus);
-//     var token = randomToken(8);
-
-//     db.verify(req.body.username, req.body.email, token);
-
-//     db.getuserid(req.body.email, function(err, result) {
-//         var id = result[0].id;
-//         var output = `<p>Dear ${username},</p>
-//             <p>Thanks for sign up. Your verification id and token is given below: </p>
-//             <ul>
-//             <li>User id: ${id}</li>
-//             <li>Token: ${token}</li>
-//             </ul>
-//             <p>Verify link: <a href="http://localhost:3000/verify">Verify</a></p>
-//             <p><br>This is automatically generated mail</p>`;
-
-//         var transporter = nodemailer.createTransport({
-//             host: "smtp.gmail.com",
-//             port: 465,
-//             secure: true,
-//             auth: {
-//                 user: "jeremykatush@gmail.com",
-//                 password: "@Jer0039 .!",
-//             },
-//         });
-
-//         var mailOptions = {
-//             from: "",
-//             to: email,
-//             subject: 'Email Verification',
-//             html: output
-//         };
-
-//         transporter.sendMail(mailOptions, function(err, info) {
-//             if (err) {
-//                 return console.log(err);
-//             }
-
-//             console.log(info);
-//         })
-
-//         res.send("Check your email for token to verify")
-//     });
-// }
 
 export const signUp = async (req, res) => {
     // Validate the request
