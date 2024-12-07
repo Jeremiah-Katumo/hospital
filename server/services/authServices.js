1c8e688bf5021fa6cd0fb4e0b96534b9e934902e
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-12-07 03:00:11 +0300

    WIP

--- server/services/authServices.js
+++ server/services/authServices.js
@@ -70,3 +70,29 @@ export const signup = async (username, email, password, status) => {
     throw error;
   }
 };
+
+
+export const matchToken = function (id, token, callback) {
+  var query = "SELECT * from `verify` WHERE token = '"+token+"' and id = " +id;
+  conn.query(query, callback);
+  console.log(query);
+}
+
+export const updateVerify = function (email, email_status, callback) {
+  var query = "UPDATE `users` SET email_status = '"+email_status+"' WHERE email = " +email;
+  conn.query(query, callback);
+  console.log(query);
+}
+
+
+export const findOne = (email, callback) => {
+  var query = "SELECT * FROM users where email = '"+email+"' ";
+  conn.query(query, callback);
+  console.log(query);
+}
+
+export const temp = (id, email, token, callback) => {
+  var query = "INSERT INTO `temp`(`id`,`email`,`token`) VALUES('"+id+"','"+email+"','"+token+"') ";
+  conn.query(query, callback);
+  console.log(query);
+}
