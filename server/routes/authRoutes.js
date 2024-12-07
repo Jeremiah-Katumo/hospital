24d1d4c5c727ed0bec58aa6b78d9299b76c7f2bb
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-12-07 18:27:00 +0300

    user authentication

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import injector from '../injectors/injector.js';
+import { AuthController } from '../controllers/AuthController.js';
+
+// Inject dependencies
+const authService = injector.get('AuthService');
+const authController = new AuthController(authService);
+
+const authRouter = express.Router();
+
+authRouter
+    .get('/register', (req, res) => authController.renderRegisterPage(req, res)) // Optional route to render a registration page
+    .post('/register', (req, res) => authController.signUp(req, res))
+    .get('/login', (req, res) => authController.renderLoginPage(req, res)) // Optional route to render a login page
+    .post('/login', (req, res) => authController.logIn(req, res))
+    .post('/verify', (req, res) => authController.verify(req, res))
+    .get('/logout', (req, res) => authController.logOut(req, res))
+    .post('/reset', (req, res) => authController.reset(req, res));
+
+export default authRouter;
