262fc642da1fb886e2aa7829c22edf7f212ef0b5
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-12-10 03:33:02 +0300

    updated schemas

--- schemas/hospital.sql
+++ schemas/hospital.sql
@@ -28,7 +28,7 @@ CREATE TABLE `appointment` (
   KEY `department_id` (`department_id`),
   KEY `doctor_id` (`doctor_id`),
   CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patient` (`id`),
-  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`),
+  CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`),
   CONSTRAINT `appointment_ibfk_3` FOREIGN KEY (`doctor_id`) REFERENCES `doctor` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -60,8 +60,8 @@ CREATE TABLE `complain` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-DROP TABLE IF EXISTS `departments`;
-CREATE TABLE `departments` (
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
@@ -74,7 +74,7 @@ CREATE TABLE `departments` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `departments` (`id`, `name`, `description`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+INSERT INTO `department` (`id`, `name`, `description`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
 (11,	'Intensive Care Unit (ICU)',	'What is an intensive care unit (ICU)? Intensive care refers to the specialised treatment given to patients who are acutely unwell and require critical medical care. An intensive care unit (ICU) provides the critical care and life support for acutely ill a',	NULL,	NULL,	'2024-12-06 20:29:45',	NULL,	NULL,	NULL),
 (16,	'Neurology',	'Neurology is a branch of medicine dealing with disorders of the nervous system. Neurology deals with the diagnosis and treatment of all categories of conditions and disease involving the central and peripheral nervous systems, including their coverings',	NULL,	NULL,	'2024-12-06 20:29:45',	NULL,	NULL,	NULL),
 (17,	'Opthalmology',	'dfvgbhjnkml',	NULL,	NULL,	'2024-12-06 20:29:45',	NULL,	NULL,	NULL),
@@ -117,7 +117,7 @@ CREATE TABLE `doctor` (
   `deleted_by` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `department_id` (`department_id`),
-  CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `departments` (`id`)
+  CONSTRAINT `doctor_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -141,8 +141,56 @@ CREATE TABLE `employee` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-DROP TABLE IF EXISTS `leaves`;
-CREATE TABLE `leaves` (
+DROP TABLE IF EXISTS `feature`;
+CREATE TABLE `feature` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` longtext NOT NULL,
+  `created_at` date DEFAULT NULL,
+  `created_by` int(10) DEFAULT NULL,
+  `updated_at` datetime DEFAULT current_timestamp(),
+  `updated_by` int(10) DEFAULT NULL,
+  `deleted_at` date DEFAULT NULL,
+  `deleted_by` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `feature` (`id`, `name`, `description`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+(1,	'appointment',	'Appointment',	NULL,	NULL,	'2024-12-10 03:24:50',	NULL,	NULL,	NULL),
+(2,	'complain',	'Complain',	NULL,	NULL,	'2024-12-10 03:27:01',	NULL,	NULL,	NULL),
+(3,	'department',	'Department',	NULL,	NULL,	'2024-12-10 03:27:18',	NULL,	NULL,	NULL),
+(4,	'diagnosis',	'Diagnosis',	NULL,	NULL,	'2024-12-10 03:27:34',	NULL,	NULL,	NULL),
+(5,	'doctor',	'Doctor',	NULL,	NULL,	'2024-12-10 03:27:48',	NULL,	NULL,	NULL),
+(6,	'employee',	'Employee',	NULL,	NULL,	'2024-12-10 03:28:03',	NULL,	NULL,	NULL),
+(7,	'hospital',	'Hospital',	NULL,	NULL,	'2024-12-10 03:28:27',	NULL,	NULL,	NULL),
+(8,	'leave',	'Leave',	NULL,	NULL,	'2024-12-10 03:28:43',	NULL,	NULL,	NULL),
+(9,	'medication',	'Medication',	NULL,	NULL,	'2024-12-10 03:29:02',	NULL,	NULL,	NULL),
+(10,	'patient',	'Patient',	NULL,	NULL,	'2024-12-10 03:29:14',	NULL,	NULL,	NULL),
+(11,	'store',	'Store',	NULL,	NULL,	'2024-12-10 03:29:27',	NULL,	NULL,	NULL),
+(12,	'user',	'User',	NULL,	NULL,	'2024-12-10 03:29:40',	NULL,	NULL,	NULL),
+(13,	'field',	'Field',	NULL,	NULL,	'2024-12-10 03:30:35',	NULL,	NULL,	NULL),
+(14,	'value',	'Value',	NULL,	NULL,	'2024-12-10 03:30:50',	NULL,	NULL,	NULL);
+
+DROP TABLE IF EXISTS `hospital`;
+CREATE TABLE `hospital` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `created_at` date DEFAULT NULL,
+  `created_by` int(10) DEFAULT NULL,
+  `updated_at` datetime DEFAULT current_timestamp(),
+  `updated_by` int(10) DEFAULT NULL,
+  `deleted_at` date DEFAULT NULL,
+  `deleted_by` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+DROP TABLE IF EXISTS `leave`;
+CREATE TABLE `leave` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
@@ -173,7 +221,7 @@ CREATE TABLE `login` (
   `created_at` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `login_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `login_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -244,6 +292,8 @@ CREATE TABLE `temp` (
   `id` int(10) NOT NULL,
   `email` varchar(255) NOT NULL,
   `token` varchar(255) NOT NULL,
+  `reset_token` varchar(255) NOT NULL,
+  `reset_token_expiry` varchar(255) NOT NULL,
   `created_at` date DEFAULT NULL,
   `created_by` int(10) DEFAULT NULL,
   `updated_at` datetime DEFAULT current_timestamp(),
@@ -253,8 +303,8 @@ CREATE TABLE `temp` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
@@ -264,6 +314,7 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL,
   `location` varchar(255) NOT NULL,
   `password` varchar(20) DEFAULT NULL,
+  `confirm_password` varchar(20) DEFAULT NULL,
   `email_status` enum('verified','not_verified') DEFAULT 'not_verified',
   `created_at` date DEFAULT NULL,
   `created_by` int(10) DEFAULT NULL,
@@ -290,8 +341,8 @@ CREATE TABLE `verify` (
   `deleted_by` int(10) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `verify_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+  CONSTRAINT `verify_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
--- 2024-12-07 17:42:54
+-- 2024-12-10 00:31:36
